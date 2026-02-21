a2991dc9d1827fe1fb7bd11420bd2277007b6bbf
gibranzizzami <gbrnzzmi24@gmail.com>
2026-02-21 21:02:15 +0700

    update

---
+++ program.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float hpp = 27000;
+    float harga_jual = 60000;
+    float diskon = 0.1;
+    float pajak_genap = 0.2;
+    float pajak_ganjil = 0.125;
+
+    /* diskon awal 10% dan akan turun setengah tiap hari sampai hari 
+    ketujuh*/
+    float hari_pertama = 0.1;
+    float hari_kedua = 0.05;
+    float hari_ketiga = 0.025;
+    float hari_keempat = 0.0125;
+    float hari_kelima = 0.00625;
+    float hari_keenam = 0.003125;
+    float hari_ketujuh = 0.0015625;
+
+    // perhitungan pendapatan dipotong pajak
+    double hari_pertama
+    double 
+}
